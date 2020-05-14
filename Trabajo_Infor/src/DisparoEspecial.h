18e5eed84a04bbfa5aca3d1cc8e7b5f81a100eaf
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-05-14 19:33:11 +0200

    Create DisparoEspecial.h

---
+++ Trabajo_Infor/src/DisparoEspecial.h
@@ -0,0 +1,13 @@
+//Práctica 6 Ismael Gómez Pacheco 53985 grupo EE309 martes 9:30-11:30
+
+#pragma once
+
+#include "Disparo.h"
+
+class DisparoEspecial :public Disparo
+{
+public:
+	DisparoEspecial();
+	virtual ~DisparoEspecial();
+	void dibuja();
+};
