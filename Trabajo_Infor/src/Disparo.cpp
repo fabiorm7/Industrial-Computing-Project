3912e8ae25f5aee1ee5f4bf4aac371c550981b3b
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-05-14 19:33:40 +0200

    Create Disparo.cpp

---
+++ Trabajo_Infor/src/Disparo.cpp
@@ -0,0 +1,43 @@
+//Práctica 6 Ismael Gómez Pacheco 53985 grupo EE309 martes 9:30-11:30
+
+#include "Disparo.h"
+#include "glut.h"
+
+Disparo::Disparo()
+{
+	radio = 0.25f;
+	velocidad.y = 10.0f;
+}
+
+Disparo::~Disparo()
+{
+}
+
+void Disparo::dibuja()
+{
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glPushMatrix();
+	glTranslatef(posicion.x, posicion.y, 0);
+	glutSolidSphere(radio, 20, 20);
+	glPopMatrix();
+
+	glDisable(GL_LIGHTING);
+	glBegin(GL_LINES);
+	glVertex3f(origen.x, origen.y, 0);
+	glVertex3f(posicion.x, posicion.y, 0);
+	glEnd();
+	glEnable(GL_LIGHTING);
+}
+
+void Disparo::setPos(Vector2D pos)
+{
+	ObjetoMovil::setPos(pos);
+	origen = pos;
+}
+
+void Disparo::setPos(float x, float y)
+{
+	posicion.x = x;
+	posicion.y = y;
+	origen = posicion;
+}
