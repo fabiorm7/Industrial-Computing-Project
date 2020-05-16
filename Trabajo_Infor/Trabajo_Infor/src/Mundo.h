9ff4ed8ab88bf27476d31a621eb4b81ebb125f83
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-05-16 02:04:34 +0200

    Create Mundo.h

---
+++ Trabajo_Infor/Trabajo_Infor/src/Mundo.h
@@ -0,0 +1,40 @@
+//Práctica 5 Ismael Gómez Pacheco 53985 grupo EE309 martes 9:30-11:30
+
+/*#include "Caja.h"
+#include "Hombre.h"
+#include "Esfera.h"
+#include "Bonus.h"
+#include "Disparo.h"
+#include "Interaccion.h"
+#include "ListaEsferas.h"
+#include "ListaDisparos.h"
+#include "EsferaPulsante.h"
+#include "DisparoEspecial.h"*/
+
+class Mundo
+{
+public: 
+	virtual ~Mundo();
+	void Tecla(unsigned char key);
+	void Inicializa();
+	void RotarOjo();
+	void Mueve();
+	void Dibuja();
+	void teclaEspecial(unsigned char key);
+	/*bool getImpacto();
+	int getNumEsferas();*/
+	bool cargarNivel();
+private:
+	float x_ojo;
+	float y_ojo;
+	float z_ojo;
+	//bool impacto;
+	int nivel;
+	/*Hombre hombre;
+	Caja caja;
+	Bonus bonus;
+	Pared plataforma;
+	ListaEsferas esferas;
+	ListaDisparos disparos;
+	DisparoEspecial disparo_especial;*/
+};
