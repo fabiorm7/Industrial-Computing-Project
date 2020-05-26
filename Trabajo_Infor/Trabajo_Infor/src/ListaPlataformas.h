e371119d3f682a668a3d5635079ef33721e8427f
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-05-26 20:46:07 +0200

    Create ListaPlataformas.h

---
+++ Trabajo_Infor/Trabajo_Infor/src/ListaPlataformas.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Pared.h"
+#include "ObjetoMovil.h"
+
+#define MAX_PLATAFORMAS 6
+
+class ListaPlataformas {
+public:
+	ListaPlataformas();
+	virtual ~ListaPlataformas();
+	bool agregar(Pared *p);
+	void dibuja();
+	void destruirContenido();
+	Pared* operator [](int i);
+	int getNumero() { return numero; }
+private:
+	Pared * lista[MAX_PLATAFORMAS];
+	int numero;
+};
