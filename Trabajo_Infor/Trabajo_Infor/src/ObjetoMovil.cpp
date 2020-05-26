9d1d5f3b801fac48ee8d7ffbd06cf6b7355c8d58
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-05-26 13:26:26 +0200

    Update ObjetoMovil.cpp

--- Trabajo_Infor/Trabajo_Infor/src/ObjetoMovil.cpp
+++ Trabajo_Infor/Trabajo_Infor/src/ObjetoMovil.cpp
@@ -34,6 +34,11 @@ void ObjetoMovil::setVel(float vx, float vy)
 	velocidad.y = vy;
 }
 
+void ObjetoMovil::setMov(float distancia)
+{
+	posicion.x += distancia;
+}
+
 Vector2D ObjetoMovil::getPos()
 {
 	return posicion;
