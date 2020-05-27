ab1b2d28a565ff50395494b126d2586c53b595e4
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-05-27 13:38:17 +0200

    Update Mundo.cpp

--- Trabajo_Infor/Trabajo_Infor/src/Mundo.cpp
+++ Trabajo_Infor/Trabajo_Infor/src/Mundo.cpp
@@ -42,6 +42,7 @@ void Mundo::Mueve()
 	for (int i = 0; i < plataformas.getNumero(); i++) {
 		Interaccion::rebote(hombre, *plataformas[i]);
 	}
+	finNivel = Interaccion::finNivel(hombre, *plataformas[6]);
 	/*bonus.mueve(0.025f);
 	disparos.mueve(0.025f);
 	esferas.mueve(0.025f);
@@ -73,19 +74,21 @@ void Mundo::Mueve()
 
 void Mundo::Inicializa()
 {
+	finNivel = false;
 	//impacto = false;
 	nivel = 0;
-	x_ojo = 0;
+	/*x_ojo = 0;
 	y_ojo = 7.5;
 	z_ojo = 30;
 	y_obs = y_ojo;
-	x_obs = z_obs = 0.0f;
+	x_obs = z_obs = 0.0f;*/
 	//bonus.setPos(5.0f, 5.0f);
 	cargarNivel();
 }
 
 Mundo::~Mundo()
 {
+	plataformas.destruirContenido();
 	/*esferas.destruirContenido();
 	disparos.destruirContenido();*/
 }
@@ -122,18 +125,20 @@ void Mundo::teclaEspecial(unsigned char key)
 	switch (key)
 	{
 	case GLUT_KEY_LEFT:
-		hombre.setVel(-5.0f, -5.0f);
-		//hombre.setMov(-1.0f);
+		hombre.setVel_x(-5.0f);
 		break;
 	case GLUT_KEY_RIGHT:
-		hombre.setVel(5.0f, -5.0f);
-		//hombre.setMov(1.0f);
+		hombre.setVel_x(5.0f);
 		break;
 	case GLUT_KEY_UP:
-		//if (hombre.getVel().y <= -9.8f) {
+		if (hombre.getPos().y == 0) {
+			hombre.setVel_y(8.0f);
+			break;
+		}
 		for (int i = 0; i < plataformas.getNumero(); i++) {
-			if (Interaccion::enPlataforma(hombre, *plataformas[i])) {
-				hombre.setVel(0.0f, 10.0f);
+			if (Interaccion::rebote(hombre, *plataformas[i])) {
+				hombre.setVel_y(8.0f);
+				break;
 			}
 		}
 		break;
@@ -155,7 +160,13 @@ int Mundo::getNumEsferas()
 bool Mundo::cargarNivel()
 {
 	nivel++;
+	x_ojo = 0;
+	y_ojo = 7.5;
+	z_ojo = 30;
+	y_obs = y_ojo;
+	x_obs = z_obs = 0.0f;
 	hombre.setPos(0, 0);
+	plataformas.destruirContenido();
 	//esferas.destruirContenido();
 	//disparos.destruirContenido();
 	if (nivel == 1)
@@ -176,9 +187,10 @@ bool Mundo::cargarNivel()
 		p4->setColor(255, 0, 0);
 		p4->setPos(13.0f, 8.0f, 18.0f, 8.0f);
 		plataformas.agregar(p4);
-		/*Esfera *e1 = new Esfera(1.5f, 2, 4, 5, 15);
-		e1->setColor(0, 0, 255);
-		esferas.agregar(e1); //esfera*/
+		Pared *puerta = new Pared();
+		puerta->setColor(0, 0, 250);
+		puerta->setPos(17.0f, 8.0f, 17.0f, 14.0f);
+		plataformas.agregar(puerta);
 	}
 	if (nivel == 2)
 	{
@@ -200,7 +212,12 @@ bool Mundo::cargarNivel()
 			esferas.agregar(aux);
 		}*/
 	}
-	if (nivel <= 3)
+	if (nivel <= 1)
 		return true;
 	return false;
 }
+
+/*bool Mundo::finNivel()
+{
+	return Interaccion::finNivel(hombre, puerta);
+}*/
