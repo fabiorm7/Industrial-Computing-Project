64fd6f37bfe08dd675115ddd33e37271b6aa4853
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-06-03 18:30:23 +0200

    Update EsferaPulsante.cpp

--- Trabajo_Infor/Trabajo_Infor/src/EsferaPulsante.cpp
+++ Trabajo_Infor/Trabajo_Infor/src/EsferaPulsante.cpp
@@ -3,14 +3,14 @@
 
 #include "EsferaPulsante.h"
 
-EsferaPulsante::EsferaPulsante()
+/*EsferaPulsante::EsferaPulsante()
 {
 	radio_max = 2.0f;
 	radio_min = 0.5f;
 	pulso = 0.5f;
 	aceleracion.y = 0.0f;
 	posicion.y = 5.0f;
-}
+}*/
 
 EsferaPulsante::~EsferaPulsante()
 {
