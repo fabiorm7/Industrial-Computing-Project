21607203930a82ef912a599fc635f4623fc7eff6
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-06-03 18:29:10 +0200

    Update Esfera.h

--- Trabajo_Infor/Trabajo_Infor/src/Esfera.h
+++ Trabajo_Infor/Trabajo_Infor/src/Esfera.h
@@ -11,11 +11,10 @@ class Esfera :public ObjetoMovil
 protected:
 	ColorRGB color;
 public:
-	Esfera();
 	Esfera(float rad, float x = 0.0f, float y = 0.0f, float vx = 0.0f, float vy = 0.0f);
 	virtual ~Esfera();
 	void dibuja();
 	void setColor(unsigned char r, unsigned char v, unsigned char a);
 	friend class Interaccion;
-	virtual Esfera* dividir();
+	//virtual Esfera* dividir();
 };
