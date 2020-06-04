52c666354171491035cf0711261f5a1c09010c03
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-06-04 13:55:52 +0200

    Create Enemigo.h

---
+++ Trabajo_Infor/Trabajo_Infor/src/Enemigo.h
@@ -0,0 +1,20 @@
+//Enemigos, son esferas que rebotan
+
+#pragma once
+
+#include "Vector2D.h"
+#include "ColorRGB.h"
+#include "ObjetoMovil.h"
+
+class Enemigo :public ObjetoMovil
+{
+protected:
+	ColorRGB color;
+public:
+	Enemigo(float rad, float x = 0.0f, float y = 0.0f, float vx = 0.0f, float vy = 0.0f);
+	virtual ~Enemigo();
+	void dibuja();
+	void setColor(unsigned char r, unsigned char v, unsigned char a);
+	friend class Interaccion;
+	//virtual Esfera* dividir();
+};
