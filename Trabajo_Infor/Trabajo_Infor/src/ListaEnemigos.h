2e5de7067cb51ac790371691682d724d4aa30cfb
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-06-04 13:57:01 +0200

    Create ListaEnemigos.h

---
+++ Trabajo_Infor/Trabajo_Infor/src/ListaEnemigos.h
@@ -0,0 +1,30 @@
+//Lista de enemigos
+
+#pragma once
+
+#include "Enemigo.h"
+#include "Caja.h"
+#include "Hombre.h"
+
+#define MAX_ENEMIGOS 100
+
+class ListaEnemigos {
+public:
+	ListaEnemigos();
+	virtual ~ListaEnemigos();
+	bool agregar(Enemigo *e);
+	void dibuja();
+	void mueve(float t);
+	void rebote(Caja caja);
+	void rebote(Pared p);
+	void rebote();
+	void destruirContenido();
+	void eliminar(int index);
+	void eliminar(Enemigo *e);
+	Enemigo* colision(Hombre &h);
+	Enemigo* operator [](int i);
+	int getNumero() { return numero; }
+private:
+	Enemigo * lista[MAX_ENEMIGOS];
+	int numero;
+};
