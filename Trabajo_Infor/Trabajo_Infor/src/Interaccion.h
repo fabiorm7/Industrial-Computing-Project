e52b3a724bb958f2fc51c63fd3a1e767c2db8730
ismagp99 <ismael.gomez.pacheco@alumnos.upm.es>
2020-06-04 13:12:05 +0200

    Update Interaccion.h

--- Trabajo_Infor/Trabajo_Infor/src/Interaccion.h
+++ Trabajo_Infor/Trabajo_Infor/src/Interaccion.h
@@ -6,6 +6,7 @@
 #include "Caja.h"
 #include "Esfera.h"
 #include "Disparo.h"
+#include "Bonus.h"
 
 class Interaccion {
 public:
@@ -16,8 +17,11 @@ public:
 	virtual ~Interaccion();
 	static bool rebote(Esfera &e, Pared p);
 	static void rebote(Esfera &e, Caja c);
+	static bool rebote(Bonus &b, Pared p);
+	static void rebote(Bonus &b, Caja c);
 	static bool rebote(Esfera& e1, Esfera& e2);
 	static bool colision(Esfera e, Hombre h);
+	static bool colision(Bonus b, Hombre h);
 	static bool colision(Disparo d, Pared p);
 	static bool colision(Disparo d, Caja c);
 	static bool colision(Disparo d, Esfera e);
